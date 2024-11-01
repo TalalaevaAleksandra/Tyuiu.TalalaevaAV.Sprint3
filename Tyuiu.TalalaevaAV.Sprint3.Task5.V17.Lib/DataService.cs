31ed4d914dedc8f1e85fb4fbdb6cc8a1464877fe
Tyuiu.TalalaevaAV@outlook.com <Tyuiu.TalalaevaAV@outlook.com>
2024-11-01 17:54:03 +0500

    Task5

---
+++ Tyuiu.TalalaevaAV.Sprint3.Task5.V17.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.TalalaevaAV.Sprint3.Task5.V17.Lib
+{
+    public class DataService : ISprint3Task5V17
+    {
+        public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
+        {
+            double sumSeries = 0;
+            int i, j;
+            for (i = startValue1; i <= stopValue1; i++)
+            {
+                for (j = startValue2; j <= stopValue2; j++)
+                {
+                    sumSeries = sumSeries + (Math.Sin(j) + x * x);
+                }
+            }
+            return Math.Round(sumSeries,3);
+        }
+    }
+}
---
+++ Tyuiu.TalalaevaAV.Sprint3.Task5.V17/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.TalalaevaAV.Sprint3.Task5.V17.Lib;
+namespace Tyuiu.TalalaevaAV.Sprint3.Task5.V17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3| Выполнила: Талалаева А.В. | ИИПб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла do-while                                           *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #17                                                             *");
+            Console.WriteLine("* Выполнил: Талалаева Александра Васильевна | ИИПБ-24-1                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Вычислить значение выражения                                            *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            int x = 2;
+            int startValue1 = 1;
+            int startValue2 = 1;
+            int stopValue1 = 3;
+            int stopValue2 = 12;
+            double res = ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(res);
+        }
+    }
+}
